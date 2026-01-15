da9c2a969b197cd9f5dc01855704a9b27f350860
Bernardo Rodrigues <107999271+BernardoKrause@users.noreply.github.com>
2026-01-15 19:14:36 -0300

    fix: fix conflict errors

--- src/main/java/com/example/ServiceController.java
+++ src/main/java/com/example/ServiceController.java
@@ -1,26 +1,38 @@
 package com.example;
 
+import com.example.entity.Service;
+import com.example.repository.ServiceRepository;
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.data.model.Page;
+import io.micronaut.data.model.Pageable;
+import io.micronaut.data.model.Sort;
+import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
+import jakarta.inject.Inject;
+import jakarta.validation.Valid;
 
-import java.util.Collection;
-import java.util.Date;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Controller("/services")
 public class ServiceController {
 
-    private final Map<UUID, Service> serviceStore = new ConcurrentHashMap<>();
+    @Inject
+    private ServiceRepository serviceRepository;
 
     public ServiceController() {
         UUID id = UUID.randomUUID();
-        serviceStore.put(id, new Service(id, "agendamento", "agendamento de algum serviço", 3499.99, new Date()));
     }
 
     @Get
     public Collection<Service> listServices() {
-        return serviceStore.values();
+        return serviceRepository.findAll();
+    }
+
+    @Post
+    public Service addService(@Body @Valid Service service) {
+        return serviceRepository.save(service);
     }
 }
