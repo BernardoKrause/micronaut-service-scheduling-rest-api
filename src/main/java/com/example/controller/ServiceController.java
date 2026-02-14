ef1789dbd702f10774ad12ccba726100092461a3
Bernardo Rodrigues <107999271+BernardoKrause@users.noreply.github.com>
2026-02-13 22:27:29 -0300

    feat: add pagination to GET endpoint

--- src/main/java/com/example/controller/ServiceController.java
+++ src/main/java/com/example/controller/ServiceController.java
@@ -1,9 +1,6 @@
 package com.example.controller;
 
 import com.example.dto.ServiceDTO;
-import com.example.entity.Service;
-import com.example.repository.ServiceRepository;
-import io.micronaut.data.model.Page;
 import io.micronaut.data.model.Pageable;
 import com.example.facade.ServiceFacade;
 import io.micronaut.http.HttpResponse;
